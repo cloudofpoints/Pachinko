8bc3579d0efc90b4dbfe7f40cb184bc26d383923
tan08 <Tim.Antrobus@sky.uk>
2015-10-15 16:55:26 +0100

    Some initial unit tests for FeatureSource. More to follow

---
+++ PachinkoTests/FeatureSourceTests.swift
@@ -0,0 +1,42 @@
+//
+//  FeatureSourceTests.swift
+//  FeatureSourceTests
+//
+//  Created by Antrobus, Tim on 12/10/2015.
+//  Copyright © 2015 cloudofpoints. All rights reserved.
+//
+
+import XCTest
+@testable import Pachinko
+
+class FeatureSourceTests: XCTestCase {
+    
+    var testFeatureSource: FeatureSource = StubFeatureSource()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testActiveFeatureHappyPath() {
+        let existingContext = FeatureContext(name: "TestContext1", synopsis: "TestContext1 Features")
+        let existingFeatureSignature = FeatureSignature(id: "001", name: "LoginFeature", synopsis: "Test login feature")
+        XCTAssertNotNil(testFeatureSource.activeFeature(existingContext, signature: existingFeatureSignature), "Existing feature \(existingFeatureSignature) should not be nil")
+    }
+    
+    func testActiveFeatureInvalidFeature() {
+        let existingContext = FeatureContext(name: "TestContext1", synopsis: "TestContext1 Features")
+        let invalidFeatureSignature = FeatureSignature(id: "999", name: "InvalidFeature", synopsis: "Invalid test feature")
+        XCTAssertNil(testFeatureSource.activeFeature(existingContext, signature: invalidFeatureSignature), "Invalid feature \(invalidFeatureSignature) should be nil")
+    }
+    
+    func testActiveFeatureInvalidContext() {
+        let invalidContext = FeatureContext(name: "InvalidContext", synopsis: "InvalidContext Features")
+        let existingFeatureSignature = FeatureSignature(id: "001", name: "LoginFeature", synopsis: "Test login feature")
+        XCTAssertNil(testFeatureSource.activeFeature(invalidContext, signature: existingFeatureSignature), "Existing feature should be nil for invalid context \(invalidContext)")
+    }
+    
+}
---
+++ PachinkoTests/StubFeatureSource.swift
@@ -0,0 +1,43 @@
+//
+//  StubFeatureSource.swift
+//  Pachinko
+//
+//  Created by Antrobus, Tim on 15/10/2015.
+//  Copyright © 2015 cloudofpoints. All rights reserved.
+//
+
+import Foundation
+@testable import Pachinko
+
+public class StubFeatureSource : FeatureSource {
+    
+    let testContextOne = FeatureContext(name: "TestContext1", synopsis: "TestContext1 Features")
+    let testContextTwo = FeatureContext(name: "TestContext2", synopsis: "TestContext2 Features")
+    let loginFeatureSignature = FeatureSignature(id: "001", name: "LoginFeature", synopsis: "Test login feature")
+    let campaignFeatureSignature = FeatureSignature(id: "002", name: "CampaignFeature", synopsis: "Test campaign feature")
+    let buttonABTestSignature_A = FeatureSignature(id: "003", name: "ButtonABTestFeature_A", synopsis: "Button A/B testing - A")
+    let buttonABTestSignature_B = FeatureSignature(id: "004", name: "ButtonABTestFeature_B", synopsis: "Button A/B testing - B")
+    
+    private lazy var featureCache: [FeatureContext : [FeatureSignature : ConditionalFeature]] = self.populateFeatureCache()
+    
+    private func populateFeatureCache() -> [FeatureContext: [FeatureSignature: ConditionalFeature]] {
+        
+        let testFeature1: ConditionalFeature = BaseFeature(signature: loginFeatureSignature, status: FeatureStatus.Active)
+        let testFeature2: ConditionalFeature = BaseFeature(signature: campaignFeatureSignature, status: FeatureStatus.Active)
+        let testFeature3: ConditionalFeature = BaseFeature(signature: buttonABTestSignature_A, status: FeatureStatus.Active)
+        let testFeature4: ConditionalFeature = BaseFeature(signature: buttonABTestSignature_B, status: FeatureStatus.Active)
+        
+        let cache = [
+            testContextOne : [loginFeatureSignature : testFeature1, campaignFeatureSignature : testFeature2],
+            testContextTwo : [buttonABTestSignature_A : testFeature3, buttonABTestSignature_B: testFeature4]
+        ]
+        return cache
+    }
+    
+    public func activeFeature(context: FeatureContext, signature: FeatureSignature) -> ConditionalFeature? {
+        guard let contextFeatures = self.featureCache[context] else {
+            return nil
+        }
+        return contextFeatures[signature]
+    }
+}
